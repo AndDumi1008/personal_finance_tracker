36d18707414097bb6b1a20f280cd0dfa22b65f40
Andrei Dumitrescu <andrei.dumitrescu100801@gmail.com>
2025-02-04 22:28:36 +0200

    + refactor table names

--- src/main/java/org/example/model/Customer.java
+++ src/main/java/org/example/model/Customer.java
@@ -14,7 +14,7 @@ import java.util.Date;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "CUSTOMER")
+@Table(name = "CUSTOMERS")
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
